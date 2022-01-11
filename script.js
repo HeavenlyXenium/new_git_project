2cd0a36caf8b8c928d233a871743de5c11213906
Heavenly <97295200+HeavenlyXenium@users.noreply.github.com>
2022-01-11 07:15:25 +0300

    Complete user request about films

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "0");
 let personalMovieDB = {
@@ -8,13 +8,37 @@ let personalMovieDB = {
 	genres: [],
 	privat: false
 };
+
 let requestAboutMovie = function() {
-	let last = prompt("Последний просмотренный вами фильм:", ""),
-		rating = prompt("Насколько оцените? (0-10)", "0");
+	let i = 0, last = prompt("Последний просмотренный вами фильм:", "");
+
+		while ( last === "" || last.length > 50 || String(+last) !== "NaN" ) {
+			alert("Некорректный ответ!");
+			last = prompt("Последний просмотренный вами фильм:", "");
+			if (++i == 2) {
+				alert("Ошибка...")
+				throw new Error("Somthing wrong!");
+			}
+		};
+
+	let rating = prompt("Насколько оцените? (0-10)", "0");
+
 		rating = rating < 0 ? 0 : rating > 10 ? 10 : typeof(+rating) == "string" ? prompt("Ошибка! Необходимо ввести число!") : rating;
 		personalMovieDB.movies[last] = rating;
 };
 
+console.log(+personalMovieDB.count)
+
+if (+personalMovieDB.count < 10) {
+	alert("Ты мало смотришь!")
+} else if (+personalMovieDB.count > 30) {
+	alert("Ты киноман!")
+} else if (String(+personalMovieDB.count) !== "NaN") {
+	alert("Ты среднячек!")
+} else {
+	alert("Ошибка проверки количества просмотренных фильмов...")
+};
+
 requestAboutMovie();
 requestAboutMovie();
-console.log(personalMovieDB.movies);
+//console.log(personalMovieDB.movies);
